7a8d36f479836da4b62f087a1f10dd7167070ac4
NingHao He <he1123783096@outlook.com>
2022-10-31 15:09:27 -0700

    HW3 Final Version

--- ImageCompression/ImageCompression.java
+++ ImageCompression/ImageCompression.java
@@ -13,6 +13,7 @@ public class ImageCompression {
 	}
 
 	JFrame frame;
+
 	JLabel lbIm1;
 	BufferedImage imgOne;
 	int width = 512; // default image width and height
@@ -21,6 +22,18 @@ public class ImageCompression {
 	double imageDWT[] = new double[3 * width * height];
 	Pixal image12[][] = new Pixal[width][height];
 	Pixal image12T[][] = new Pixal[width][height];
+
+	// create an array that stors 9 image12T
+	Pixal image12T0[][] = new Pixal[width][height];
+	Pixal image12T1[][] = new Pixal[width][height];
+	Pixal image12T2[][] = new Pixal[width][height];
+	Pixal image12T3[][] = new Pixal[width][height];
+	Pixal image12T4[][] = new Pixal[width][height];
+	Pixal image12T5[][] = new Pixal[width][height];
+	Pixal image12T6[][] = new Pixal[width][height];
+	Pixal image12T7[][] = new Pixal[width][height];
+	Pixal image12T8[][] = new Pixal[width][height];
+	Pixal image12T9[][] = new Pixal[width][height];
 
 	/**
 	 * Read Image RGB
@@ -53,22 +66,22 @@ public class ImageCompression {
 					int newR = Byte.toUnsignedInt(r);
 					int newG = Byte.toUnsignedInt(g);
 					int newB = Byte.toUnsignedInt(b);
-					image1[(x + y * width) * 3] = newR;
-					image1[(x + y * width) * 3 + 1] = newG;
-					image1[(x + y * width) * 3 + 2] = newB;
 					image12[x][y] = new Pixal();
 					image12[x][y].r = (double) newR;
 					image12[x][y].g = (double) newG;
 					image12[x][y].b = (double) newB;
+					image12T9[x][y] = new Pixal();
+					image12T9[x][y].r = (double) newR;
+					image12T9[x][y].g = (double) newG;
+					image12T9[x][y].b = (double) newB;
+					image12T[x][y] = new Pixal();
+					image12T[x][y].r = (double) newR;
+					image12T[x][y].g = (double) newG;
+					image12T[x][y].b = (double) newB;
 					ind++;
 
 				}
 			}
-			// copy image1 to imageDWT
-			for (int i = 0; i < image1.length; i++) {
-				imageDWT[i] = (double) image1[i];
-			}
-			// imageDWT = Doubles.toArray(Integer.asList(image1));
 			encodingImage(imageDWT, img, level);
 
 		} catch (FileNotFoundException e) {
@@ -78,55 +91,347 @@ public class ImageCompression {
 		}
 	}
 
-	public void encodingImage(double[] image1, BufferedImage img, int level) {
-		int iniLevel = 9;
-		int need = iniLevel - level;
-		int widthT = width;
-		int heightT = height;
-		int initWidth = 0;
-		int initHeight = 0;
-		for (int x = 0; x < need; x++) {
-			// calculateCol(widthT, heightT);
-			// calculateLevel(initWidth, initHeight, widthT, heightT, img);
-			calculateLevel2(initWidth, initHeight, widthT, heightT);
-			widthT = widthT / 2;
-			heightT = heightT / 2;
-			initHeight = initHeight + heightT;
-			// widthT = widthT / 2;
-			// heightT = heightT / 2;
-			// initWidth = initWidth + widthT;
-			// initHeight = initHeight + heightT;
-		}
-		// calculateLevel2(widthT, heightT);
-		// calculateLevel2(widthT, heightT);
-		System.out.println("done");
-		// loop through the image
+	private void zeroOut(Pixal[][] Image) {
 		for (int y = 0; y < height; y++) {
 			for (int x = 0; x < width; x++) {
-				if (image12T[x][y].isHigh) {
-					image12T[x][y].r = 0;
-					image12T[x][y].g = 0;
-					image12T[x][y].b = 0;
+
+				if (Image[x][y].isHigh) {
+					Image[x][y].r = 0;
+					Image[x][y].g = 0;
+					Image[x][y].b = 0;
 
 				}
-				// // int pix = (a << 24) | (r << 16) | (g << 8) | b;
-				// // img.setRGB(x, y, pix);
 			}
 		}
-		// decodingImage(widthT, heightT);
-		System.out.println(widthT);
-		System.out.println(heightT);
-		for (int x = 0; x < need; x++) {
-			decodingImage2(widthT, heightT, need);
-			widthT = widthT * 2;
-			heightT = heightT * 2;
+	}
+
+	public void encodingImage(double[] image1, BufferedImage img, int level) {
+		frame = new JFrame();
+		if (level == -1) {
+			int need = 9;
+			int widthT = width;
+			int heightT = height;
+			int initWidth = 0;
+			int initHeight = 0;
+			int needT = 9;
+			for (int x = 0; x < need; x++) {
+				calculateLevel2(initWidth, initHeight, widthT, heightT);
+				widthT = widthT / 2;
+				heightT = heightT / 2;
+				initHeight = initHeight + heightT;
+
+				// store the image into the array
+
+				for (int i = 0; i < width; i++) {
+					for (int j = 0; j < height; j++) {
+						if (x == 0) {
+							image12T8[i][j] = new Pixal();
+							image12T8[i][j].r = image12T[i][j].r;
+							image12T8[i][j].g = image12T[i][j].g;
+							image12T8[i][j].b = image12T[i][j].b;
+							image12T8[i][j].isHigh = image12T[i][j].isHigh;
+						} else if (x == 1) {
+							image12T7[i][j] = new Pixal();
+							image12T7[i][j].r = image12T[i][j].r;
+							image12T7[i][j].g = image12T[i][j].g;
+							image12T7[i][j].b = image12T[i][j].b;
+							image12T7[i][j].isHigh = image12T[i][j].isHigh;
+						} else if (x == 2) {
+							image12T6[i][j] = new Pixal();
+							image12T6[i][j].r = image12T[i][j].r;
+							image12T6[i][j].g = image12T[i][j].g;
+							image12T6[i][j].b = image12T[i][j].b;
+							image12T6[i][j].isHigh = image12T[i][j].isHigh;
+						} else if (x == 3) {
+							image12T5[i][j] = new Pixal();
+							image12T5[i][j].r = image12T[i][j].r;
+							image12T5[i][j].g = image12T[i][j].g;
+							image12T5[i][j].b = image12T[i][j].b;
+							image12T5[i][j].isHigh = image12T[i][j].isHigh;
+						} else if (x == 4) {
+							image12T4[i][j] = new Pixal();
+							image12T4[i][j].r = image12T[i][j].r;
+							image12T4[i][j].g = image12T[i][j].g;
+							image12T4[i][j].b = image12T[i][j].b;
+							image12T4[i][j].isHigh = image12T[i][j].isHigh;
+						} else if (x == 5) {
+							image12T3[i][j] = new Pixal();
+							image12T3[i][j].r = image12T[i][j].r;
+							image12T3[i][j].g = image12T[i][j].g;
+							image12T3[i][j].b = image12T[i][j].b;
+							image12T3[i][j].isHigh = image12T[i][j].isHigh;
+						} else if (x == 6) {
+							image12T2[i][j] = new Pixal();
+							image12T2[i][j].r = image12T[i][j].r;
+							image12T2[i][j].g = image12T[i][j].g;
+							image12T2[i][j].b = image12T[i][j].b;
+							image12T2[i][j].isHigh = image12T[i][j].isHigh;
+						} else if (x == 7) {
+							image12T1[i][j] = new Pixal();
+							image12T1[i][j].r = image12T[i][j].r;
+							image12T1[i][j].g = image12T[i][j].g;
+							image12T1[i][j].b = image12T[i][j].b;
+							image12T1[i][j].isHigh = image12T[i][j].isHigh;
+						} else if (x == 8) {
+							image12T0[i][j] = new Pixal();
+							image12T0[i][j].r = image12T[i][j].r;
+							image12T0[i][j].g = image12T[i][j].g;
+							image12T0[i][j].b = image12T[i][j].b;
+							image12T0[i][j].isHigh = image12T[i][j].isHigh;
+						}
+					}
+				}
+			}
+			// widthT = width;
+			// heightT = height;
+			for (int i = 0; i < need; i++) {
+				if (i == 0) {
+					zeroOut(image12T0);
+				} else if (i == 1) {
+					zeroOut(image12T1);
+				} else if (i == 2) {
+					zeroOut(image12T2);
+				} else if (i == 3) {
+					zeroOut(image12T3);
+				} else if (i == 4) {
+					zeroOut(image12T4);
+				} else if (i == 5) {
+					zeroOut(image12T5);
+				} else if (i == 6) {
+					zeroOut(image12T6);
+				} else if (i == 7) {
+					zeroOut(image12T7);
+				} else if (i == 8) {
+					zeroOut(image12T8);
+				}
+			}
+			for (int i = 0; i < need + 1; i++) {
+				if (i == 9) {
+					for (int x = 0; x < width; x++) {
+						for (int y = 0; y < height; y++) {
+							image12T[x][y].r = image12T9[x][y].r;
+							image12T[x][y].g = image12T9[x][y].g;
+							image12T[x][y].b = image12T9[x][y].b;
+						}
+					}
+				}
+				if (i == 6) {
+					for (int x = 0; x < width; x++) {
+						for (int y = 0; y < height; y++) {
+							image12T[x][y].r = image12T6[x][y].r;
+							image12T[x][y].g = image12T6[x][y].g;
+							image12T[x][y].b = image12T6[x][y].b;
+						}
+					}
+					int widthtemp = widthT;
+					int heighttemp = heightT;
+					for (int x = i; x < 9; x++) {
+
+						decodingImage2(widthtemp, heighttemp, need);
+						widthtemp = widthtemp * 2;
+						heighttemp = heighttemp * 2;
+					}
+				}
+				if (i == 5) {
+					for (int x = 0; x < width; x++) {
+						for (int y = 0; y < height; y++) {
+							image12T[x][y].r = image12T5[x][y].r;
+							image12T[x][y].g = image12T5[x][y].g;
+							image12T[x][y].b = image12T5[x][y].b;
+						}
+					}
+					int widthtemp = widthT;
+					int heighttemp = heightT;
+					for (int x = i; x < 9; x++) {
+
+						decodingImage2(widthtemp, heighttemp, need);
+						widthtemp = widthtemp * 2;
+						heighttemp = heighttemp * 2;
+					}
+				}
+				if (i == 4) {
+					for (int x = 0; x < width; x++) {
+						for (int y = 0; y < height; y++) {
+							image12T[x][y].r = image12T4[x][y].r;
+							image12T[x][y].g = image12T4[x][y].g;
+							image12T[x][y].b = image12T4[x][y].b;
+						}
+					}
+					int widthtemp = widthT;
+					int heighttemp = heightT;
+					for (int x = i; x < 9; x++) {
+
+						decodingImage2(widthtemp, heighttemp, need);
+						widthtemp = widthtemp * 2;
+						heighttemp = heighttemp * 2;
+					}
+				}
+				if (i == 3) {
+					for (int x = 0; x < width; x++) {
+						for (int y = 0; y < height; y++) {
+							image12T[x][y].r = image12T3[x][y].r;
+							image12T[x][y].g = image12T3[x][y].g;
+							image12T[x][y].b = image12T3[x][y].b;
+						}
+					}
+					int widthtemp = widthT;
+					int heighttemp = heightT;
+					for (int x = i; x < 9; x++) {
+
+						decodingImage2(widthtemp, heighttemp, need);
+						widthtemp = widthtemp * 2;
+						heighttemp = heighttemp * 2;
+					}
+				}
+				if (i == 2) {
+					for (int x = 0; x < width; x++) {
+						for (int y = 0; y < height; y++) {
+							image12T[x][y].r = image12T2[x][y].r;
+							image12T[x][y].g = image12T2[x][y].g;
+							image12T[x][y].b = image12T2[x][y].b;
+						}
+					}
+					int widthtemp = widthT;
+					int heighttemp = heightT;
+					for (int x = i; x < 9; x++) {
+
+						decodingImage2(widthtemp, heighttemp, need);
+						widthtemp = widthtemp * 2;
+						heighttemp = heighttemp * 2;
+					}
+				}
+				if (i == 1) {
+					for (int x = 0; x < width; x++) {
+						for (int y = 0; y < height; y++) {
+							image12T[x][y].r = image12T1[x][y].r;
+							image12T[x][y].g = image12T1[x][y].g;
+							image12T[x][y].b = image12T1[x][y].b;
+						}
+					}
+					int widthtemp = widthT;
+					int heighttemp = heightT;
+					for (int x = i; x < 9; x++) {
+
+						decodingImage2(widthtemp, heighttemp, need);
+						widthtemp = widthtemp * 2;
+						heighttemp = heighttemp * 2;
+					}
+				}
+				if (i == 7) {
+					for (int x = 0; x < width; x++) {
+						for (int y = 0; y < height; y++) {
+							image12T[x][y].r = image12T7[x][y].r;
+							image12T[x][y].g = image12T7[x][y].g;
+							image12T[x][y].b = image12T7[x][y].b;
+						}
+					}
+					int widthtemp = widthT;
+					int heighttemp = heightT;
+					for (int x = i; x < 9; x++) {
+
+						decodingImage2(widthtemp, heighttemp, need);
+						widthtemp = widthtemp * 2;
+						heighttemp = heighttemp * 2;
+					}
+					// decodingImage2(widthT, heightT, need);
+				}
+				if (i == 8) {
+					for (int x = 0; x < width; x++) {
+						for (int y = 0; y < height; y++) {
+							image12T[x][y].r = image12T8[x][y].r;
+							image12T[x][y].g = image12T8[x][y].g;
+							image12T[x][y].b = image12T8[x][y].b;
+						}
+					}
+					int widthtemp = widthT;
+					int heighttemp = heightT;
+					for (int x = i; x < 9; x++) {
+
+						decodingImage2(widthtemp, heighttemp, need);
+						widthtemp = widthtemp * 2;
+						heighttemp = heighttemp * 2;
+					}
+					// drawImage2(img, need);
+				}
+				if (i == 0) {
+					for (int x = 0; x < width; x++) {
+						for (int y = 0; y < height; y++) {
+							image12T[x][y].r = image12T0[x][y].r;
+							image12T[x][y].g = image12T0[x][y].g;
+							image12T[x][y].b = image12T0[x][y].b;
+						}
+					}
+					int widthtemp = widthT;
+					int heighttemp = heightT;
+					for (int x = i; x < 9; x++) {
+
+						decodingImage2(widthtemp, heighttemp, need);
+						widthtemp = widthtemp * 2;
+						heighttemp = heighttemp * 2;
+					}
+					// drawImage2(img, need);
+				}
+
+				widthT = widthT * 2;
+				heightT = heightT * 2;
+				// wait 2 seconds
+
+				drawImage2(img, need);
+				try {
+					Thread.sleep(1500);
+					frame.remove(lbIm1);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+			// for (int x = 0; x < width; x++) {
+			// for (int y = 0; y < height; y++) {
+			// image12T[x][y].r = image12T8[x][y].r;
+			// image12T[x][y].g = image12T8[x][y].g;
+			// image12T[x][y].b = image12T8[x][y].b;
+			// }
+			// }
+			// decodingImage2(widthT, heightT, need);
+
+		} else {
+			int iniLevel = 9;
+			int need = iniLevel - level;
+			int widthT = width;
+			int heightT = height;
+			int initWidth = 0;
+			int initHeight = 0;
+			for (int x = 0; x < need; x++) {
+				calculateLevel2(initWidth, initHeight, widthT, heightT);
+				widthT = widthT / 2;
+				heightT = heightT / 2;
+				initHeight = initHeight + heightT;
+			}
+			System.out.println("done");
+			// loop through the image
+			for (int y = 0; y < height; y++) {
+				for (int x = 0; x < width; x++) {
+
+					if (image12T[x][y].isHigh) {
+						image12T[x][y].r = 0;
+						image12T[x][y].g = 0;
+						image12T[x][y].b = 0;
+
+					}
+				}
+			}
+			for (int x = 0; x < need; x++) {
+
+				decodingImage2(widthT, heightT, need);
+				widthT = widthT * 2;
+				heightT = heightT * 2;
+			}
+
+			drawImage(img, need);
 		}
 
-		drawImage(img, need);
 	}
 
 	public void decodingImage2(int widthT, int heightT, int need) {
-
 		for (int x = 0; x < width; x++) {
 			for (int y = 0; y < height; y++) {
 				image12[x][y].r = image12T[x][y].r;
@@ -137,7 +442,6 @@ public class ImageCompression {
 		}
 		// 128 * 2 - 1 = 255
 		int d = height - 1;
-		System.out.println("d: " + d);
 		// heightT = height - heightT * 2;
 		for (int y = height - 1; y >= height - heightT; y--) {
 			for (int x1 = 0; x1 < widthT; x1++) {
@@ -190,8 +494,73 @@ public class ImageCompression {
 
 			}
 		}
-		System.out.println(widthT);
-		System.out.println(heightT);
+	}
+
+	public void decodingImageFinal(int widthT, int heightT, Pixal[][] image12TDA) {
+
+		for (int x = 0; x < width; x++) {
+			for (int y = 0; y < height; y++) {
+				image12[x][y].r = image12T3[x][y].r;
+				image12[x][y].g = image12T3[x][y].g;
+				image12[x][y].b = image12T3[x][y].b;
+				image12[x][y].isHigh = image12T3[x][y].isHigh;
+			}
+		}
+		System.out.println("widthT " + widthT);
+		// 128 * 2 - 1 = 255
+		int d = height - 1;
+		// heightT = height - heightT * 2;
+		for (int y = height - 1; y >= height - heightT; y--) {
+			for (int x1 = 0; x1 < widthT; x1++) {
+				Pixal averagePixal = image12[x1][y];
+				Pixal differencePixal = image12[x1][y - heightT];
+
+				// Pixal differencePixal2 = image12T[x1][y - heightT + 1];
+				// int newP = (y + 1) / 2;
+				int newP = d;
+				image12T3[x1][newP].r = averagePixal.r - differencePixal.r;
+				image12T3[x1][newP].g = averagePixal.g - differencePixal.g;
+				image12T3[x1][newP].b = averagePixal.b - differencePixal.b;
+				int newP2 = d - 1;
+				// int newP = (y + 1) / 2;
+				image12T3[x1][newP2].r = averagePixal.r + differencePixal.r;
+				image12T3[x1][newP2].g = averagePixal.g + differencePixal.g;
+				image12T3[x1][newP2].b = averagePixal.b + differencePixal.b;
+
+				// image12T[x1][y - heightT + 1].r = averagePixal2.r + differencePixal2.r;
+				// image12T[x1][y - heightT + 1].g = averagePixal2.g + differencePixal2.g;
+				// image12T[x1][y - heightT + 1].b = averagePixal2.b + differencePixal2.b;
+
+			}
+			d = d - 2;
+		}
+		for (int x = 0; x < width; x++) {
+			for (int y = 0; y < height; y++) {
+				image12[x][y].r = image12T3[x][y].r;
+				image12[x][y].g = image12T3[x][y].g;
+				image12[x][y].b = image12T3[x][y].b;
+				image12[x][y].isHigh = image12T3[x][y].isHigh;
+			}
+		}
+		// widthT = widthT * 2;
+		int rheightT = height - heightT * 2;
+		for (int y = height - 1; y >= rheightT; y--) {
+			for (int x1 = 0; x1 < widthT; x1++) {
+				Pixal averagePixal = image12[x1][y];
+				Pixal differencePixal = image12[x1 + widthT][y];
+
+				// Pixal differencePixal2 = image12T[x1][y - heightT + 1];
+				int newP = 2 * (x1);
+				image12T3[newP][y].r = averagePixal.r + differencePixal.r;
+				image12T3[newP][y].g = averagePixal.g + differencePixal.g;
+				image12T3[newP][y].b = averagePixal.b + differencePixal.b;
+				int newP2 = 2 * (x1) + 1;
+				image12T3[newP2][y].r = averagePixal.r - differencePixal.r;
+				image12T3[newP2][y].g = averagePixal.g - differencePixal.g;
+				image12T3[newP2][y].b = averagePixal.b - differencePixal.b;
+
+			}
+		}
 	}
 
 	public void calculateLevel2(int initWidth, int initHeight, int widthT, int heightT) {
@@ -243,22 +612,6 @@ public class ImageCompression {
 		for (int y = 0; y < height; y++) {
 			for (int x = 0; x < width; x++) {
 				int a = 0;
-				// if (need == 0) {
-				// double r = image1[(x + y * width) * 3];
-				// double g = image1[(x + y * width) * 3 + 1];
-				// double b = image1[(x + y * width) * 3 + 2];
-				// int pix = 0xff000000 | (((int) r & 0xff) << 16) | (((int) g & 0xff) << 8) |
-				// ((int) b & 0xff);
-				// img.setRGB(x, y, pix);
-				// } else {
-				// // System.out.println("x: " + x + " y: " + y);
-				// double r = imageDWT[(x + y * width) * 3];
-				// double g = imageDWT[(x + y * width) * 3 + 1];
-				// double b = imageDWT[(x + y * width) * 3 + 2];
-				// int pix = 0xff000000 | (((int) r & 0xff) << 16) | (((int) g & 0xff) << 8) |
-				// ((int) b & 0xff);
-				// img.setRGB(x, y, pix);
-				// }
 				double r = image12T[x][y].r;
 				double g = image12T[x][y].g;
 				double b = image12T[x][y].b;
@@ -266,6 +619,40 @@ public class ImageCompression {
 				img.setRGB(x, y, pix);
 			}
 		}
+	}
+
+	public void drawImage2(BufferedImage img, int need) {
+		for (int y = 0; y < height; y++) {
+			for (int x = 0; x < width; x++) {
+				int a = 0;
+				double r = image12T[x][y].r;
+				double g = image12T[x][y].g;
+				double b = image12T[x][y].b;
+				int pix = 0xff000000 | (((int) r & 0xff) << 16) | (((int) g & 0xff) << 8) | ((int) b & 0xff);
+				img.setRGB(x, y, pix);
+
+			}
+		}
+		//
+		GridBagLayout gLayout = new GridBagLayout();
+		frame.getContentPane().setLayout(gLayout);
+
+		lbIm1 = new JLabel(new ImageIcon(imgOne));
+
+		GridBagConstraints c = new GridBagConstraints();
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.anchor = GridBagConstraints.CENTER;
+		c.weightx = 0.5;
+		c.gridx = 0;
+		c.gridy = 0;
+
+		c.fill = GridBagConstraints.HORIZONTAL;
+		c.gridx = 0;
+		c.gridy = 1;
+		frame.getContentPane().add(lbIm1, c);
+		frame.pack();
+		frame.setVisible(true);
+
 	}
 
 	public void calculateCol(int initW, int initH, int widthT, int heightT) {
@@ -306,28 +693,30 @@ public class ImageCompression {
 		// Read in the specified image
 		imgOne = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
 		readImageRGB(width, height, args[0], imgOne, level);
+		if (level != -1) {
+			frame = new JFrame();
+			GridBagLayout gLayout = new GridBagLayout();
+			frame.getContentPane().setLayout(gLayout);
 
+			lbIm1 = new JLabel(new ImageIcon(imgOne));
+
+			GridBagConstraints c = new GridBagConstraints();
+			c.fill = GridBagConstraints.HORIZONTAL;
+			c.anchor = GridBagConstraints.CENTER;
+			c.weightx = 0.5;
+			c.gridx = 0;
+			c.gridy = 0;
+
+			c.fill = GridBagConstraints.HORIZONTAL;
+			c.gridx = 0;
+			c.gridy = 1;
+			frame.getContentPane().add(lbIm1, c);
+
+			frame.pack();
+			frame.setVisible(true);
+		}
 		// Use label to display the image
-		frame = new JFrame();
-		GridBagLayout gLayout = new GridBagLayout();
-		frame.getContentPane().setLayout(gLayout);
 
-		lbIm1 = new JLabel(new ImageIcon(imgOne));
-
-		GridBagConstraints c = new GridBagConstraints();
-		c.fill = GridBagConstraints.HORIZONTAL;
-		c.anchor = GridBagConstraints.CENTER;
-		c.weightx = 0.5;
-		c.gridx = 0;
-		c.gridy = 0;
-
-		c.fill = GridBagConstraints.HORIZONTAL;
-		c.gridx = 0;
-		c.gridy = 1;
-		frame.getContentPane().add(lbIm1, c);
-
-		frame.pack();
-		frame.setVisible(true);
 	}
 
 	public static void main(String[] args) {
--- ImageCompression/ReadMe-Java.txt
+++ ImageCompression/ReadMe-Java.txt
@@ -1,13 +1,12 @@
 A sample program to read and display an image in JavaFX panels. By default, this program will read in the first frame of a given .rgb video file.
 
 
-Unzip the folder to where you want.
 To run the code from command line, first compile with:
 
->> javac ImageDisplay.java
+>> javac ImageCompression.java
 
-and then, you can run it to take in two parameters "pathToRGBVideo" and "n":
+and then, you can run it to take in two parameters "image.rgb" and "n":
 
->> java ImageDisplay pathToRGBVideo n
+>> java ImageCompression image.rgb n
 
 where the parameter "n" is a "dummy" parameter used to give an additional example of command line args in Java.
